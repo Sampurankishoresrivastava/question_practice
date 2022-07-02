f7cf734795a85cd1d2ccf494c79480a7f924e84b
Sampuran Kishore Srivastava <90759917+Sampurankishoresrivastava@users.noreply.github.com>
2022-07-02 23:03:41 +0530

    Create CHANGEPOS.cpp

---
+++ CHANGEPOS.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int sx,sy,ex,ey;
+	    cin>>sx>>sy>>ex>>ey;
+	    if(sx!=ex and sy!=ey){
+	        cout<<1<<endl;
+	    }
+	    else{
+	        cout<<2<<endl;
+	    }
+	}
+	return 0;
+}
